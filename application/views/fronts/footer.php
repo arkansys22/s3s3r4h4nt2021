0ae995d4b3b3870183912623b833f22c811b5549
arkans <82564511+arkansys22@users.noreply.github.com>
2021-10-17 18:37:43 +0700

    up

--- application/views/fronts/footer.php
+++ application/views/fronts/footer.php
@@ -3,10 +3,13 @@
 	<div class="top_footer">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                	<div class="footer_logo">
-                    	<a href="index-12.html"><img alt="logo" src="demo-shop/images/shop-logo_white.png"></a>
-                    </div>
+							<div class="col-lg-3 col-md-6">
+								<div class="footer_logo">
+										<a href="<?php echo base_url()?>"><img height="250px"alt="logo" src="<?php echo base_url()?>assets/frontend/campur/<?php echo $identitas->logo?>"></a>
+									</div>
+							</div>
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+
                     <p><?php echo $identitas->meta_deskripsi?></p>
                     <ul class="contact_info contact_info_light list_none">
                         <li>
@@ -23,7 +26,7 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
                 	<h6 class="widget_title">Syarat & Ketentuan</h6>
                     <ul class="list_none widget_links">
                     	<li><a href="#">Pendaftaran</a></li>
@@ -31,7 +34,7 @@
                       <li><a href="#">Catatan Penting!</a></li>
                     </ul>
                 </div>
-                <div class="col-lg-4 col-md-6">
+                <div class="col-lg-3 col-md-6">
                     <h6 class="widget_title">SOSMED</h6>
                     <ul class="list_none footer_social">
                     	  <li><a href="<?php echo $identitas->facebook?>"><i class="ion-social-facebook"></i></a></li>
